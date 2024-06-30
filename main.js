7096a8ccdc1cc400a8b7cd4a066e1f2eaec43d45
fuaadabdulahi <fuaadabdulahi5@gmail.com>
2024-06-30 08:59:41 +0300

    three files

---
+++ main.js
@@ -0,0 +1,14 @@
+let a =  10;
+let b = 5;
+console.log(a+b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+/*post-increment*/
+console.log(a++);
+console.log(a);
+//pre-decrement
+console.log(--b);
+//post increment
+console.log(b--);
+console.log(b);
