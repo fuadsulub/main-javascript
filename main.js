d879dc6a9a1475d9986abfceaaa2e769ec48bbc0
fuaadabdulahi <fuaadabdulahi5@gmail.com>
2024-07-06 07:55:13 +0300

    updated two files

--- main.js
+++ main.js
@@ -26,6 +26,74 @@ console.log(++x);
 console.log(--y)
 console.log(x%y)
 
+// comparison operators
+
+// equal to
+let w = 10;
+let z = "10";
+
+ console.log(w == z);
+
+// // not equal to
+ console.log(w != z);
+
+// // strictly equal to
+ console.log(w === z);
+
+// // strictly not equal
+ console.log(w !== z);
+
+// greater than 
+console.log(w > z);
+
+// less than
+console.log(w < z);
+
+// greater than or equal
+console.log(w >= z);
+
+// less than or equal
+console.log( w <= z);
+
+
+let c = 15;
+let d = "15";
+console.log(c == d);
+console.log(c != d);
+console.log(c === d);
+console.log(c !== d);
+console.log(c > d);
+console.log(c < d);
+console.log(c >= d)
+console.log(c <= d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
